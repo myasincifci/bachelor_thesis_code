5ab065b411ba963e19427ddc369bbb60164b32fb
Mehmet Yasin Cifci <cifci@campus.tu-berlin.de>
2023-06-17 12:18:31 +0200

    added download script for pretrained models

---
+++ model_zoo/download_models.py
@@ -0,0 +1,11 @@
+import gdown
+import tarfile
+
+url = 'https://drive.google.com/u/0/uc?id=1IsnAlys_Bu3mgSd8Iji7TcwJqkq8uotB&export=download'
+output = './model_zoo/models.tar.xz'
+
+if __name__ == "__main__":
+    gdown.download(url, output, quiet=False)
+
+    with tarfile.open('./model_zoo/models.tar.xz') as f:
+        f.extractall('./model_zoo')
