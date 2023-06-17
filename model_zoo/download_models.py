7cbea76cb1f49c18423f2c0f25c5eb73e429d4fb
Mehmet Yasin Cifci <cifci@campus.tu-berlin.de>
2023-06-17 12:38:10 +0200

    added res50 models

--- model_zoo/download_models.py
+++ model_zoo/download_models.py
@@ -2,6 +2,7 @@ import gdown
 import tarfile
 
 url = 'https://drive.google.com/u/0/uc?id=1IsnAlys_Bu3mgSd8Iji7TcwJqkq8uotB&export=download'
+
 output = './model_zoo/models.tar.xz'
 
 if __name__ == "__main__":
